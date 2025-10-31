3e352ed1005f2f3bdd45bca160144908e23ab4b9
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-10-31 23:05:17 +0700

    setup worker struct

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,14 @@
 package worker
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/louishuyng/orchestrator/task"
+)
+
+type Worker struct {
+	ID        uuid.UUID
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
