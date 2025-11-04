92fd79854404726a4e5d6ed048324b20d7dbfa12
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-11-04 20:04:31 +0700

    implement stats

---
+++ worker/stats.go
@@ -0,0 +1,53 @@
+package worker
+
+import "github.com/c9s/goprocinfo/linux"
+
+type Stats struct {
+	MemStats  *linux.MemInfo
+	DiskStats *linux.Disk
+	CpuStats  *linux.CPUStat
+	LoadStats *linux.LoadAvg
+}
+
+// Memory stats
+func (s *Stats) MemTotalKb() uint64 {
+	return s.MemStats.MemTotal
+}
+
+func (s *Stats) MemAvailableKb() uint64 {
+	return s.MemStats.MemAvailable
+}
+
+func (s *Stats) MemUsedKb() uint64 {
+	return s.MemStats.MemTotal - s.MemStats.MemAvailable
+}
+
+func (s *Stats) MemUsedPercent() uint64 {
+	return s.MemAvailableKb() / s.MemTotalKb()
+}
+
+// Disk stats
+func (s *Stats) DiskTotal() uint64 {
+	return s.DiskStats.All
+}
+
+func (s *Stats) DiskFree() uint64 {
+	return s.DiskStats.Free
+}
+
+func (s *Stats) DiskUsed() uint64 {
+	return s.DiskStats.Used
+}
+
+// CPU stats
+func (s *Stats) CpuUsage() float64 {
+	idle := s.CpuStats.Idle + s.CpuStats.IOWait
+	nonIdle := s.CpuStats.User + s.CpuStats.Nice + s.CpuStats.System + s.CpuStats.SoftIRQ + s.CpuStats.Steal
+	total := idle + nonIdle
+
+	if total == 0 {
+		return 0.0
+	}
+
+	return (float64(total) - float64(idle)) / float64(total)
+}
